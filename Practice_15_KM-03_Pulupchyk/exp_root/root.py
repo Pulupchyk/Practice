cafdfb59cb772ecb9f238e8d68c6097ad4acc7f1
Pulupchyk <72132903+Pulupchyk@users.noreply.github.com>
2020-12-24 17:16:39 +0200

    Add files via upload

---
+++ Practice_15_KM-03_Pulupchyk/exp_root/root.py
@@ -0,0 +1,5 @@
+def root2(n):
+    return round(pow(n, 1/2), 3)
+
+def root3(n):
+    return round(pow(n, 1/3), 3)
---
+++ Practice_15_KM-03_Pulupchyk/logarithm/logarithm.py
@@ -0,0 +1,8 @@
+import math
+
+def log(a, b):
+    return round(math.log(b, a), 2)
+def ln(b):
+    return round(math.log(b), 2)
+def lg(b):
+    return round(math.log(b, 10), 2)
