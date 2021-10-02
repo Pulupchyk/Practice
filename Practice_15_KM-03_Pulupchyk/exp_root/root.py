2e292ed820a9eab6f69b20751033d285458271a6
Horny_Artem <72132903+Pulupchyk@users.noreply.github.com>
2021-10-02 16:46:11 +0300

    Delete Practice_15_KM-03_Pulupchyk directory

--- Practice_15_KM-03_Pulupchyk/exp_root/root.py
+++
@@ -1,5 +0,0 @@
-def root2(n):
-    return round(pow(n, 1/2), 3)
-
-def root3(n):
-    return round(pow(n, 1/3), 3)
--- Practice_15_KM-03_Pulupchyk/logarithm/logarithm.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def log(a, b):
-    return round(math.log(b, a), 2)
-def ln(b):
-    return round(math.log(b), 2)
-def lg(b):
-    return round(math.log(b, 10), 2)
